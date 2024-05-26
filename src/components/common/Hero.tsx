b281bec5d095356d586f502d09694ec3a3dbd700
Chuqdi <morganhezekiah111@gmail.com>
2024-05-26 18:48:44 +0100

    g

--- src/components/common/Hero.tsx
+++ src/components/common/Hero.tsx
@@ -7,7 +7,7 @@ function Hero() {
     <div className="bg-black_hero_bg pl-[1rem] md:pl-[13rem] pb-[3rem] flex  items-center flex-col md:flex-row">
       <div className="z-10 w-full pt-[9rem] md:pt-0 md:w-[100.625rem] flex flex-col gap-[1.5rem] px-[1rem] md:px-0">
         <div className="flex flex-col font-Bricolage text-white text-[2.5rem] md:text-[4.5rem] font-[800] tracking-[-0.045rem]">
-        <h3 className="">
+        <h3 className="md:pt-40">
           AI-Driven Real
         </h3>
         <h3 className="primary_text">Assets Tokenization</h3>
