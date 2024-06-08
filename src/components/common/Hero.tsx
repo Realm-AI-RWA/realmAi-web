a000c6a3bad02c1309112ae6229b05becf37b410
Chuqdi <morganhezekiah111@gmail.com>
2024-06-08 21:22:05 +0100

    g

--- src/components/common/Hero.tsx
+++ src/components/common/Hero.tsx
@@ -11,7 +11,7 @@ function Hero() {
 
 
   function countDown() {
-    let targetDate = new Date('2024-06-14T00:00:00.000Z');
+    let targetDate = new Date('2024-06-16T00:00:00.000Z');
      timerRef.current = setInterval(() => {
       let difference = targetDate.getTime() - new Date().getTime();
       if (difference <= 0) {
